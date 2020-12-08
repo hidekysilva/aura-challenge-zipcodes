bb0ea0e5b43aa6323e037c9ceb03643704c1ce66
Hideky Silva <hidekysilva@Hidekys-MBP.home>
2020-12-08 16:14:22 -0500

    added some tests

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const { processZipRequest } = require("./routes/zip");
 // lambda-like handler function
 module.exports.handler = async (event) => {
   const results = processEvent(event);
-  console.log(results.length);
-  return "success";
+  return results;
 };
 
 const processEvent = (event) => {
@@ -37,36 +36,3 @@ const getRouteProps = (name) => {
   if (!routes) return null;
   return routes;
 };
-
-/* 
-  Data length 2296
-  Some area_code values are null... falsy
-  Some population values are 0... falsy
-  To find multiple elements based on a condition use Array.filter
-  Required to search by zip code, city and latitude/longitude
-  Let's add search by area code, population and county
-  Min length of primary city is 3 characters
-  Min length of county is 4 + 7 characters. All end with " County"
-  Let's limit search of partial zip code and city to 3 characters minimum
-*/
-
-// const lengthOfCityName = (l) => data.filter((d) => d.longitude === null);
-
-// console.log(findRecordByZip("01002"));
-
-// console.log(lengthOfCityName(3).length);
-
-// console.log(data.length);
-
-// console.log(validateEventObject({ httpMethod: "GET", path: "/zip" }));
-
-// console.log(
-//   getRecords(validateQueryStringParams({ zip: "090", latitude: "42.0" }))
-// );
-
-// console.log(
-//   getRecords(validateQueryStringParams({ zip: 0, estimated_population: 0 }))
-//     .length
-// );
-
-// console.log(getRecords({ zip: "0106", estimated_population: 0 }));
--- src/test.js
+++ src/test.js
@@ -1,13 +1,140 @@
 const { handler } = require("./index");
+const data = require("./data.json");
 
 describe("basic tests", () => {
   test("handler function exists", () => {
     expect(typeof handler).toBe("function");
   });
 
-  test("handler returns success", async () => {
+  test("handler returns array of length data.length", async () => {
     await expect(
       handler({ httpMethod: "GET", path: "/zip", queryStringParameters: {} })
-    ).resolves.toEqual("success");
+    ).resolves.toHaveLength(data.length);
+  });
+
+  test("handler throws when given invalid event", async () => {
+    await expect(handler("event")).rejects.toThrow();
+  });
+
+  test("handler throws when given invalid httpMethod", async () => {
+    await expect(
+      handler({
+        httpMethod: "FIND",
+        path: "/zip",
+        queryStringParameters: { zip: "010" },
+      })
+    ).rejects.toThrow();
+  });
+
+  test("handler throws when given invalid path", async () => {
+    await expect(
+      handler({
+        httpMethod: "GET",
+        path: "/zap",
+        queryStringParameters: { zip: "010" },
+      })
+    ).rejects.toThrow();
+  });
+
+  test("handler throws when given invalid queryStringParameters", async () => {
+    await expect(
+      handler({
+        httpMethod: "GET",
+        path: "/zip",
+        queryStringParameters: null,
+      })
+    ).rejects.toThrow();
+  });
+
+  test("handler returns empty array", async () => {
+    await expect(
+      handler({
+        httpMethod: "GET",
+        path: "/zip",
+        queryStringParameters: { zip: "99999" },
+      })
+    ).resolves.toEqual([]);
+  });
+
+  test("handler throws when zip param is less than 3 characters", async () => {
+    await expect(
+      handler({
+        httpMethod: "GET",
+        path: "/zip",
+        queryStringParameters: { zip: "01" },
+      })
+    ).rejects.toThrow();
+  });
+
+  test("handler returns non-empty array when given a valid zip param", async () => {
+    const result = await handler({
+      httpMethod: "GET",
+      path: "/zip",
+      queryStringParameters: { zip: "061" },
+    });
+
+    await expect(Array.isArray(result)).toBe(true);
+
+    await expect(result.length).toBeGreaterThan(0);
+  });
+
+  test("handler returns non-empty array when given a valid primary_city param", async () => {
+    const result = await handler({
+      httpMethod: "GET",
+      path: "/zip",
+      queryStringParameters: { primary_city: "New Haven" },
+    });
+
+    await expect(Array.isArray(result)).toBe(true);
+
+    await expect(result.length).toBeGreaterThan(0);
+  });
+
+  test("handler returns non-empty array when given a valid county param", async () => {
+    const result = await handler({
+      httpMethod: "GET",
+      path: "/zip",
+      queryStringParameters: { county: "Worcester" },
+    });
+
+    await expect(Array.isArray(result)).toBe(true);
+
+    await expect(result.length).toBeGreaterThan(0);
+  });
+
+  test("handler returns non-empty array when given a valid latitude param", async () => {
+    const result = await handler({
+      httpMethod: "GET",
+      path: "/zip",
+      queryStringParameters: { latitude: "42.0" },
+    });
+
+    await expect(Array.isArray(result)).toBe(true);
+
+    await expect(result.length).toBeGreaterThan(0);
+  });
+
+  test("handler returns non-empty array when given a valid longitude param", async () => {
+    const result = await handler({
+      httpMethod: "GET",
+      path: "/zip",
+      queryStringParameters: { longitude: "-72.0" },
+    });
+
+    await expect(Array.isArray(result)).toBe(true);
+
+    await expect(result.length).toBeGreaterThan(0);
+  });
+
+  test("handler returns non-empty array when given a valid estimated_population param", async () => {
+    const result = await handler({
+      httpMethod: "GET",
+      path: "/zip",
+      queryStringParameters: { estimated_population: "900" },
+    });
+
+    await expect(Array.isArray(result)).toBe(true);
+
+    await expect(result.length).toBeGreaterThan(0);
   });
 });
